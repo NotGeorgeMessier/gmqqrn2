71f35e3a906781b1f318529cd18c03d22986b372
Andrey Kologreev <andrey>
2024-03-09 05:48:03 +0400

    wip

--- src/screens/ExampleScreen.tsx
+++ src/screens/ExampleScreen.tsx
@@ -1,9 +1,12 @@
 import {BaseView, UIText} from "@src/com";
 import {MAIN_SCREEN, TStackScreen} from "@src/navigation";
 import * as React from "react";
+import {getModel} from "react-native-device-info";
 import {ScrollView} from "react-native-gesture-handler";
 
 export const ExampleScreen: TStackScreen<typeof MAIN_SCREEN> = () => {
+    getModel();
+
     return (
         <BaseView>
             <ScrollView>

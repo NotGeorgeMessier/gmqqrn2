553aa196d189878c3a9d33332ad79a6d7e0b2648
Andrey Kologreev <andrey>
2024-03-09 04:22:51 +0400

    wip

--- App.tsx
+++ App.tsx
@@ -1,28 +1,18 @@
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import React from "react";
+import {SafeAreaView, Text} from "react-native";
 
 function App(): React.JSX.Element {
-  return (
-    <SafeAreaView>
-      <Text style={[styles.sectionTitle]}>{'title'}</Text>
-    </SafeAreaView>
-  );
+    return (
+        <SafeAreaView>
+            <Text style={styles.sectionTitle}>{"title"}</Text>
+        </SafeAreaView>
+    );
 }
 
 const styles = {
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
+    sectionTitle: {
+        fontSize: 24,
+    },
 };
 
 export default App;
